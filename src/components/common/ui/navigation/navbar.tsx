3a758619f06271fa3b92e341c0227aa59c471d82
Tulza <40633228+tulza@users.noreply.github.com>
2024-09-27 17:50:38 +1200

    Refactored navigation bar

---
+++ src/components/common/ui/navigation/navbar.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import { Info, Keyboard, Settings, User } from 'lucide-react'
+
+import { TLink } from '../transition/TLink'
+import { Dropdown } from '../wrapper/dropdown'
+
+export const NavigationBar = () => {
+  return (
+    <div className="flex absolute w-full items-center justify-between">
+      <TLink href="/">
+        <div className="flex gap-4 font-kollektif items-center p-2">
+          <Keyboard size={28} />
+          <h1 className="text-3xl">KeyLabs</h1>
+        </div>
+      </TLink>
+      <div className="flex gap-4 p-2">
+        <TLink href="/login" className="p-2 border rounded-md bg-white">
+          login wow
+        </TLink>
+        <Info size={28} />
+        <Settings size={28} />
+        <Dropdown>
+          <User size={28} />
+        </Dropdown>
+      </div>
+    </div>
+  )
+}
