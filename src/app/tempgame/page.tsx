c227d4ff89ae9e0ffc96b11193e7597914f5f496
Anton Garay <antonvince.garay@gmail.com>
2024-09-27 22:55:10 +1200

    asda

--- src/app/tempgame/page.tsx
+++ src/app/tempgame/page.tsx
@@ -5,10 +5,7 @@ import GameTestClient from './GameTestClient'
 export const dynamic = 'force-dynamic'
 
 const GameTestPage = async () => {
-  // Server-side logic to get the user
   const user = await getUser()
-
-  // Pass user as a prop to the client component
   return <GameTestClient user={user} />
 }
 
