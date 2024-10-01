0fb9a1dd4e36ead3a8c439a81a4ebf4d75c75ed8
Tulza <40633228+tulza@users.noreply.github.com>
2024-10-02 00:30:25 +1300

    Add new Keylabs icon

---
+++ src/components/common/KeyLabsLogo.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+
+const KeyLabsIcon = ({
+  className,
+  stroke,
+}: {
+  className?: string;
+  stroke?: string;
+}) => {
+  return (
+    <svg
+      className={className}
+      width="21"
+      height="21"
+      viewBox="0 0 21 21"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g clipPath="url(#clip0_76_105)">
+        <path
+          d="M8.76611 7.43555H8.77329"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10.4326 10.7688H10.4398"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M12.0991 7.43555H12.1063"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M5.43262 7.43555H5.43979"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M6.26611 14.1021L10.4328 14.1021"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M7.09912 10.7688H7.1063"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M10.8491 17.4354H3.76579C2.84531 17.4354 2.09912 16.6892 2.09912 15.7687V5.76872C2.09912 4.84824 2.84531 4.10205 3.76579 4.10205H17.0991C18.0196 4.10205 18.7658 4.84824 18.7658 5.76872V12.4354"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+        <path
+          d="M13.2076 19.6521H13.1326C13.076 19.6334 13.0265 19.5979 12.9908 19.5502C12.955 19.5025 12.9347 19.445 12.9326 19.3855V11.0521C12.9326 10.9784 12.9613 10.9076 13.0126 10.8547C13.064 10.8018 13.1339 10.771 13.2076 10.7688C13.2743 10.7688 13.3326 10.7938 13.3826 10.8355L19.4993 15.8355C19.5448 15.871 19.578 15.92 19.5941 15.9754C19.6102 16.0309 19.6084 16.09 19.5891 16.1444C19.5697 16.1988 19.5337 16.2457 19.4862 16.2785C19.4387 16.3113 19.382 16.3283 19.3243 16.3271H15.5826L13.441 19.5355C13.4138 19.5717 13.3786 19.6011 13.3381 19.6213C13.2976 19.6416 13.2529 19.6521 13.2076 19.6521Z"
+          stroke={stroke}
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </g>
+      <defs>
+        <clipPath id="clip0_76_105">
+          <rect
+            width="20"
+            height="20"
+            transform="translate(0.432617 0.768799)"
+          />
+        </clipPath>
+      </defs>
+    </svg>
+  );
+};
+
+export default KeyLabsIcon;
--- src/components/common/ui/navigation/keylabslogo.tsx
+++ src/components/common/ui/navigation/keylabslogo.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Keyboard } from 'lucide-react';
 
+import KeyLabsIcon from '../../KeyLabsLogo';
 import TLink from '../transition/TLink';
 
 export const Keylabslogo = () => {
   return (
     <TLink href="/">
       <div className="flex gap-4 font-kollektif items-center p-2">
-        <Keyboard size={28} />
+        <KeyLabsIcon className="stroke-black size-8" />
         <h1 className="text-3xl select-none">KeyLabs</h1>
       </div>
     </TLink>
