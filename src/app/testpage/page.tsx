fd1b8ba0cc85c49b1b844f1b5f20aad8c3651fe6
Tulza <40633228+tulza@users.noreply.github.com>
2024-10-07 00:21:14 +1300

    add testpage

---
+++ src/app/testpage/page.tsx
@@ -0,0 +1,11 @@
+import { Leaderboard } from '@/components/Leaderboard';
+
+const Testpage = () => {
+  return (
+    <div className="flex items-center justify-center">
+      <Leaderboard />
+    </div>
+  );
+};
+
+export default Testpage;
--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -5,8 +5,8 @@ import Button from './common/Button';
 export const Leaderboard = () => {
   return (
     <>
-      <div className="fixed left-0 top-0 z-[98] h-dvh w-dvw bg-black/70" />
-      <div className="fixed left-1/2 top-1/2 z-[99] rounded-xl bg-background px-9 py-4 text-white [translate:-50%_-50%]">
+      <div className="fixed left-0 top-0 z-[98] h-dvh w-dvw bg-black/50" />
+      <div className="fixed left-1/2 top-1/2 z-[99] rounded-xl bg-background px-9 py-4 text-foreground [translate:-50%_-50%]">
         <div className="flex w-full justify-between whitespace-nowrap text-4xl">
           <p className="w-min">Leaderboards</p>
           <p className="w-min">English</p>
@@ -21,14 +21,14 @@ export const Leaderboard = () => {
           </div>
         </div>
         <div className="flex gap-8">
-          <div className="w-[400px] odd:*:text-background even:*:bg-white">
+          <div className="w-[400px] odd:*:text-background even:*:bg-highlight">
             <Score />
             <Score />
             <Score />
             <Score />
             <Score />
           </div>
-          <div className="w-[400px] odd:*:text-background even:*:bg-white">
+          <div className="w-[400px] odd:*:text-background even:*:bg-highlight">
             <Score />
             <Score />
             <Score />
