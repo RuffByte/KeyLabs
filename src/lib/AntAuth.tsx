a1e06797d8ee40dd3f501f11fba613c9497e5ffe
Anton Garay <antonvince.garay@gmail.com>
2024-10-08 23:21:31 +1300

    Feature: Added new Auth functions

--- src/lib/AntAuth.tsx
+++ src/lib/AntAuth.tsx
@@ -1,26 +1,95 @@
+import { cookies } from 'next/headers';
+import { sha256 } from '@oslojs/crypto/sha2';
+import {
+  encodeBase32,
+  encodeBase32LowerCaseNoPadding,
+  encodeHexLowerCase,
+} from '@oslojs/encoding';
 import type { Session, User } from '@prisma/client';
 
+import { prisma } from './prisma';
+
+//generates session token
 export function generateSessionToken(): string {
-  // TODO
+  const bytes = new Uint8Array(20);
+  crypto.getRandomValues(bytes);
+  const token = encodeBase32LowerCaseNoPadding(bytes);
+  return token;
 }
 
 export async function createSession(
   token: string,
-  userId: number
+  userId: string
 ): Promise<Session> {
-  // TODO
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const session: Session = {
+    id: sessionId,
+    userId,
+    expiresAt: new Date(Date.now() + 1000 * 60 * 60 * 24 * 30),
+  };
+  await prisma.session.create({
+    data: session,
+  });
+  return session;
 }
 
 export async function validateSessionToken(
   token: string
 ): Promise<SessionValidationResult> {
-  // TODO
+  const sessionId = encodeHexLowerCase(sha256(new TextEncoder().encode(token)));
+  const result = await prisma.session.findUnique({
+    where: {
+      id: sessionId,
+    },
+    include: {
+      user: true,
+    },
+  });
+  if (result === null) {
+    return { session: null, user: null };
+  }
+  const { user, ...session } = result;
+  if (Date.now() >= session.expiresAt.getTime()) {
+    await prisma.session.delete({
+      where: {
+        id: session.id,
+      },
+    });
+    return { session: null, user: null };
+  }
+  if (Date.now() >= session.expiresAt.getTime() - 1000 * 60 * 60 * 24 * 15) {
+    session.expiresAt = new Date(Date.now() + 1000 * 60 * 60 * 24 * 30);
+    await prisma.session.update({
+      where: {
+        id: session.id,
+      },
+      data: {
+        expiresAt: session.expiresAt,
+      },
+    });
+  }
+  return { session, user };
 }
 
 export async function invalidateSession(sessionId: string): Promise<void> {
-  // TODO
+  await prisma.session.delete({
+    where: {
+      id: sessionId,
+    },
+  });
 }
 
 export type SessionValidationResult =
   | { session: Session; user: User }
   | { session: null; user: null };
+
+  function handleRequest(request: HTTPRequest, response: HTTPResponse): void {
+    if (request.method !== "GET") {
+      const origin = request.headers.get("Origin");
+      // You can also compare it against the Host or X-Forwarded-Host header.
+      if (origin === null || origin !== "https://example.com") {
+        response.setStatusCode(403);
+        return;
+      }
+    }
+  
